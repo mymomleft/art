bb3d390e937abc00abcd1024a2e0ef92f372dc93
potatoman1 <55966798+mymomleft@users.noreply.github.com>
2025-03-24 22:02:43 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
